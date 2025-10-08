79520cc988e3d82cf58e7af5f35ef03a798a74e9
Wang Zekun <yeweiyang96@icloud.com>
2025-10-08 15:28:09 +0900

    chore: 更新VSCode配置，移除不再需要的扩展，调整mcp的环境变量，添加CI工作流

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('G4Vista-Web');
+  protected readonly title = signal('G4Vista');
 }
