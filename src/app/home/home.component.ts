54ee1b6b0ddf4baf0d0bbb66d675671ca25ca977
Wang Zekun <yeweiyang96@icloud.com>
2025-11-12 11:44:47 +0900

    feat: 添加主页组件及其样式，更新路由配置

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-home',
+  imports: [LogoComponent, MatDividerModule, MatIconModule, MatButtonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
